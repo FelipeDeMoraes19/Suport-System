e609cea56c512cb2422d276f2ff8026d222945c4
FelipeDeMoraes19 <moorraes16@gmail.com>
2025-02-12 01:00:08 -0300

    Update das URLS

--- apps/ticket/urls.py
+++ apps/ticket/urls.py
@@ -6,9 +6,9 @@ from .views import (
 app_name = 'ticket'
 
 urlpatterns = [
+    path('', DashboardView.as_view(), name='dashboard'), 
     path('suporte-ticket/', CreateTicketView.as_view(), name='create'),
-    path('tickets/', DashboardView.as_view(), name='dashboard'),
     path('tickets/<int:ticket_id>/', TicketDetailView.as_view(), name='ticket_detail'),
-    path('tickets/<int:ticket_id>/enviar_mensagem/', TicketDetailView.as_view(), name='send_message'),  
+    path('tickets/<int:ticket_id>/enviar_mensagem/', TicketDetailView.as_view(), name='send_message'),
     path('login/', CustomLoginView.as_view(), name='login'),
 ]
--- conf/urls.py
+++ conf/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-
+from django.shortcuts import redirect
 from .settings import STATIC_URL, STATIC_ROOT
 
+from apps.ticket.views import DashboardView
+
 urlpatterns = [
-    path('', include('apps.landingpage.urls', namespace='landingpage')),
+    path('', DashboardView.as_view(), name='dashboard'), 
+
     path('base/', include('apps.base.urls', namespace='base')),
     path('ceo-painel/', admin.site.urls),
     path('ticket/',  include('apps.ticket.urls', namespace='ticket')),
